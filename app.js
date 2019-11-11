aafcbd3dee1b22277b4b7746016fb70385ef40d4
Craig Shaw <cshaw@cccneb.edu>
2019-11-11 10:34:18 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const http = require('http') //imports the http module from Node
+const express = require('express')  // imports the express module 
+const app = express()
+
+app.get('/', function(req, res){
+  res.end('Home Page')
+
+})
+
+app.get('/about', function(req, res){
+    res.end('About Page')
+    
+  })
+
+app.get('/craig', function(req, res){
+    res.end('Craig Page')
+    
+  })
+
+app.use(function(req, res){
+    res.statusCode = 404
+    res.end('Not Found')
+    
+  }) 
+
+const server = http.createServer(app)
+server.listen(3000)
+
